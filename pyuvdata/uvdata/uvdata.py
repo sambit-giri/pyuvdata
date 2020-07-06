15a583631a71f14eddceaca8f2d225e239417eaf
Chris Moriarty <cmoriarty@gmail.com>
2020-07-06 10:41:27 -0400

    Update uvdata.py
    Simple code comment change to test permissions.

--- pyuvdata/uvdata/uvdata.py
+++ pyuvdata/uvdata/uvdata.py
@@ -29,7 +29,7 @@ class UVData(UVBase):
     """
     A class for defining a radio interferometer dataset.
 
-    Currently supported file types: uvfits, miriad, fhd.
+    Currently supported file types: uvfits, miriad, fhd, mir.
     Provides phasing functions.
 
     Attributes
